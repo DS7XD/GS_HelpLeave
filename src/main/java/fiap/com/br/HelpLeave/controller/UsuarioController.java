8f8e07768ef031254cda1cc1c8430df46bc216a9
DS7XD <lucasmpp7@gmail.com>
2025-05-29 05:01:23 -0300

    d

--- src/main/java/fiap/com/br/HelpLeave/controller/UsuarioController.java
+++ src/main/java/fiap/com/br/HelpLeave/controller/UsuarioController.java
@@ -1,14 +1,8 @@
 package fiap.com.br.HelpLeave.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import fiap.com.br.HelpLeave.model.Usuario;
 import fiap.com.br.HelpLeave.repository.UsuarioRepository;
 
--- src/main/java/fiap/com/br/HelpLeave/repository/AlertaRepository.java
+++ src/main/java/fiap/com/br/HelpLeave/repository/AlertaRepository.java
@@ -1,7 +1,6 @@
 package fiap.com.br.HelpLeave.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import fiap.com.br.HelpLeave.model.Alerta;
 
 public interface AlertaRepository extends JpaRepository<Alerta, Long> {
