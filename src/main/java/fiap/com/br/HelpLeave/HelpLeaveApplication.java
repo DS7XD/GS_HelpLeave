d5f6d5f5383f4b1f62758c30705de16d51183d6f
DS7XD <lucasmpp7@gmail.com>
2025-05-29 04:08:44 -0300

    a

---
+++ src/main/java/fiap/com/br/HelpLeave/HelpLeaveApplication.java
@@ -0,0 +1,13 @@
+package fiap.com.br.HelpLeave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpLeaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpLeaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fiap/com/br/HelpLeave/HelpLeaveApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.com.br.HelpLeave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpLeaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
