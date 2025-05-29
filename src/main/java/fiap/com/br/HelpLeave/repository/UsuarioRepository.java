168665291555a7a7f01c601b4e834c255deba615
DS7XD <lucasmpp7@gmail.com>
2025-05-29 12:13:26 -0300

    j

--- src/main/java/fiap/com/br/HelpLeave/repository/UsuarioRepository.java
+++ src/main/java/fiap/com/br/HelpLeave/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package fiap.com.br.HelpLeave.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +11,5 @@ import fiap.com.br.HelpLeave.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Page<Usuario> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
 
-    Object findByEmail(String email);
+    Optional<Usuario> findByEmail(String email);
 }
